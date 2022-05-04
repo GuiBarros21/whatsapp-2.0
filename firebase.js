0379a8c3e72f29d9d93654eba7e75f7de14712a3
Guilherme Barros <barros.guilherme21@gmail.com>
2022-05-04 01:10:33 -0700

    Initial Commit
    Commit after a few days of work

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAOgcegYZw7nA0UTAZmxyc9hcxeXUbg8Nk",
+    authDomain: "whatsapp-2-e8435.firebaseapp.com",
+    projectId: "whatsapp-2-e8435",
+    storageBucket: "whatsapp-2-e8435.appspot.com",
+    messagingSenderId: "603330114134",
+    appId: "1:603330114134:web:6b51d6b44abf22641d01f7",
+    measurementId: "G-K17YJLGJXZ"
+};
+
+const app = !firebase.apps.length
+? firebase.initializeApp(firebaseConfig) 
+: firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {db,auth,provider} ;
